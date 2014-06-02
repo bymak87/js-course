03a233cd38880a83639c5e344a391b661f1343cf
Amy Bakameyer <bymak87@gmail.com>
2014-06-01 19:48:15 -0500

    completed exercise 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 alert('hi');
+
+var itemTemplate = $('#templates .item').html();
+var newItemHtml = Robin.render(itemTemplate, {name: 'Dewberry', price: 0.15});
+$('#store').append(newItemHtml);
+
+var items=[
+{name: 'Apple', price: 0.90},
+{name: 'Carrots', price: 1},
+{name: 'Pear', price: 0.95}
+];
+
+items.forEach(function(item){
+  var x = item.name;
+  var y = item.price;
+  var newItems = Robin.render(itemTemplate, {name: item.name, price: item.price});
+$('#store').append(newItems);
+});
+
